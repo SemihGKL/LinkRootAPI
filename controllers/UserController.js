052216dcf41fc596a226b8344c66a14bcbbd5f69
Semih Gokol <gokol.semih@gmail.com>
2022-07-30 10:55:38 +0200

    implémentation de la route addUser

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,33 +1,39 @@
 //On devra appeler mongoose pour communiquer avec mongoDB
-var mongoose = require('require');
+var mongoose = require('mongoose');
 
 //Importer notre modèle pour s'appuyer dessus lors des différentes actions
 var UserModel = require('../models/UserModel');
 
 //On implémente la partie pour hasher le mdp
-var bcrypt = require('bcrypt')
+// var bcrypt = require('bcrypt');
 SALT_WORK_FACTOR = 10;
 
 
 //Fonction d'ajout d'un user
-function addUser(username, email, password) {
-    //On crée une instance de notre modèle pour le remplir
-    let hashPassword = password; //va falloir hasher ça
+function addUser(req, res) {
 
+    let hashPassword = req.body.password; //va falloir hasher ça
+
+    //On crée une instance de notre modèle pour le remplir
     let user = new UserModel({
-        username: username,
-        email: email,
+        username: req.body.username,
+        email: req.body.email,
         password: hashPassword
     })
     //On enregistre le user
     user.save();
 
+    if (res.status(201) != "Created") {
+        console.log("erreur dans l'ajout du user")
+    } else {
+        console.log("l'user a été ajouté")
+    }
 }
 
 //Fonction de modification d'un user
-function updateUser(UserId, newUsername, newEmail, newPassword) {
+function updateUser(req, res) {
     //On va créer un requère et mettre un await pour lors de l'exec() attendre 
-    let updatedUser = searchUser(UserId);
+    let updatedUser = searchUser(res.param);
 
     //Puis modifier ses datas
     updatedUser.username = newUsername;
@@ -35,16 +41,22 @@ function updateUser(UserId, newUsername, newEmail, newPassword) {
     updatedUser.password = newPassword;
 
     //l'await permet d'attendre que tous les traitements précédents soient terminés
-    await updateUser.save();
+    updatedUser.save();
 }
 
 //Fonction de recherche d'un User
-function searchUser(UserId) {
-    let user = await UserModel.findById({_id: UserId}).exec();
+function searchUser (UserId) {
+    let user = UserModel.findById({_id: UserId}).exec();
     return user;
 }
 
 //Login
 function loginUser() {
 
+}
+
+module.exports = {
+    addUser,
+    updateUser,
+    loginUser
 }
--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -2,17 +2,19 @@ const { default: mongoose } = require('mongoose');
 
 const router = require('express').Router();
 
+//On importe toutes nos méhodes provenant du controller sous forme de variables
+const  { 
+    addUser,
+    updatedUser,
+    loginUser
+} = require('../controllers/UserController.js')
+
+
 //route put asynchrone car pas immédiat (connexion db etc)
 router.put('/', async (req, res) => {
     
 })
 
-router.post('/login', async (req, res) => {
-    
-})
-
-router.put('/', async (req, res) => {
-    
-})
+router.post('/addUser',addUser)
 
 module.exports = router;
